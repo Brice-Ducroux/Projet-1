14fe7af00fcdc57530ed76d1661e683206219618
kouaouci <karimakouaouci2803@gmail.com>
2021-10-07 17:07:58 +0200

    correction fonction carossel

--- js/style.js
+++ js/style.js
@@ -1,35 +1,38 @@
 /* fonction de la carossell*/
-var arrayslide =[{ phrase:"la reflexiologie dans tout ces etat", img:"../img/pied-accueil4.jpg"},   { phrase:"lorsque on masse les pieds ,on caresse notre âme",
-img:"../img/pieds2.jpg"},{ phrase:"les pieds miroir de notre corps", img:"../img/piedsaccueil3.jpg"},]
-var myImg = document.getElementById("img");
+var arrayslide = [
+  {
+    phrase: "la reflexiologie dans tout ces etat",
+    img: "../img/pied-accueil4.jpg",
+  },
+  {
+    phrase: "lorsque on masse les pieds ,on caresse notre âme",
+    img: "../img/pieds2.jpg",
+  },
+  {
+    phrase: "les pieds miroir de notre corps",
+    img: "../img/piedsaccueil3.jpg",
+  },
+];
+var myImg = document.getElementById("imgCaroussel");
 var myTittle = document.getElementById("phrase");
-var i=0;
-function slidshownext(){
-    myImg.src=arrayslide[i].img;
-    myTittle.textContent=arrayslide[i].phrase;
-
-    if (i<arrayslide.length-1){
-        i++;
-    }
-    else{
-        i=0
-    }
-
-
+var i = 0;
+function slidshownext() {
+  if (i < arrayslide.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  myImg.src = arrayslide[i].img;
+  myTittle.textContent = arrayslide[i].phrase;
 }
 
-function slidwhowprevious(){
-    myImg.src=arrayslide[i].img;
-    myTittle.textContent=arrayslide.phrase
-    ;
-
-
-if(i>0){i--
-
-}
-else
-{i=arrayslide.length-1;
-}
-
+function slidshowprevious() {
+  if (i > 0) {
+    i--;
+  } else {
+    i = arrayslide.length - 1;
+  }
+  myImg.src = arrayslide[i].img;
+  myTittle.textContent = arrayslide.phrase;
 }
 /*menu burger*/
